f582c1a91e637eb4ab518a5a15e5c3227cff8e9a
Victor Tsang <victorwctsang@gmail.com>
2023-02-12 16:03:33 +1100

    Initial commit

---
+++ PerformSimulations.R
@@ -0,0 +1,109 @@
+# Simulation Experiments
+# Get estimates using simulated datasets
+
+library(logger)
+
+source('helpers-simulation-experiments.R')
+
+log_info("Loading synthetic dataset and configuration")
+
+load("data/synthetic-data.RData")
+attach(synthetic.data.config)
+
+alpha = 0.05
+
+methods.point_estimates = c(
+  "Strauss",
+  "MLE",
+  "BA-MLE"
+)
+methods.conf_int = c(
+  "MINMI",
+  "GRIWM",
+  "GRIWM-corrected"
+)
+
+RESULTS_PATH = "data/simResults.RData"
+
+results = data.frame(
+  error_factor=double(),
+  method=factor(),
+  lower=double(),
+  point=double(),
+  upper=double(),
+  point_runtime=double(),
+  conf_int_runtime=double(),
+  B.lower=double(),
+  B.point=double(),
+  B.upper=double()
+)
+
+pilot.dates = datasets[1, "W"][[1]]
+A = 0.2 * (mean(fossil.sd)) ^ 2
+
+############################################################
+# Run Trials
+############################################################
+log_info("Performing Simulations")
+
+start_time = Sys.time()
+for (i in 1:nrow(datasets)) {
+  log_info(sprintf("Dataset: %i/%i", i, nrow(datasets)))
+  iter = datasets[i, ]
+  W = as.numeric(iter$W[[1]])
+  sd = as.numeric(iter$error_factor * fossil.sd)
+  
+  log_info("Getting point estimates")
+  for (method in methods.point_estimates) {
+    estimation = estimate_extinction(
+      W = W,
+      sd = sd,
+      method = method,
+      K = K,
+      dating_error.mean = dating_error.mean
+    )
+    log_info(sprintf("Time taken for %s: %.02f seconds", method, estimation$point_runtime))
+    
+    results = tibble::add_row(
+      results,
+      error_factor = iter$error_factor,
+      method = method,
+      point = estimation$point,
+      point_runtime = estimation$point_runtime,
+      conf_int_runtime = estimation$conf_int_runtime
+    )
+  }
+  
+  log_info("Getting confidence intervals")
+  for (method in methods.conf_int) {
+    estimation = estimate_conf_int(
+      W = W,
+      sd = sd,
+      method = method,
+      alpha = alpha,
+      K = K,
+      dating_error.mean = dating_error.mean
+    )
+    log_info(sprintf("Time taken for %s: %.02f seconds", method, estimation$conf_int_runtime))
+    results = tibble::add_row(
+      results,
+      error_factor = iter$error_factor,
+      method = method,
+      lower = estimation$lower,
+      point = estimation$point,
+      upper = estimation$upper,
+      point_runtime = estimation$point_runtime,
+      conf_int_runtime = estimation$conf_int_runtime,
+      B.lower = estimation$B.lower,
+      B.point = estimation$B.point,
+      B.upper = estimation$B.upper
+    )
+  }
+}
+sims.time = Sys.time() - start_time
+log_info(sims.time)
+
+## Save results
+log_info(sprintf("Saving results to %s", RESULTS_PATH))
+save(results, file = RESULTS_PATH)
+View(results)
---
+++ helpers-simulation-experiments.R
@@ -0,0 +1,187 @@
+
+library(readxl)
+
+source("GRIWM.R")
+
+getFossilData = function (path, sheet, range, col_names, col_types) {
+  read_excel(path, sheet, range, col_names, col_types)
+}
+
+boundFossilData = function (fossil.df, K) {
+  return(fossil.df[fossil.df$age < K,])
+}
+
+getStdDevFromFossilData = function (path, K, n, ...) {
+  df = getFossilData(path, ...)
+  df = boundFossilData(df, K)
+  return(df$sd[1:n])
+}
+
+create_result_df = function (n.sims, method, error_factor) {
+  n.methods = length(method)
+  n.error_factors = length(error_factor)
+  df.length = n.sims * n.methods * n.error_factors
+  
+  data.frame(
+    sim_id = rep(1:n.sims, each = (n.methods * n.error_factors)),
+    method = rep(method, each = n.error_factors, length.out = df.length),
+    error_factor = rep(error_factor, length.out = df.length),
+    lower = rep(NA, df.length),
+    upper = rep(NA, df.length),
+    mean = rep(NA, df.length),
+    width = rep(NA, df.length),
+    contains_theta = rep(NA, df.length),
+    compute_time = rep(NA, df.length)
+  )
+}
+
+simulate_dataset = function (theta.true,
+                             K,
+                             eps.mean,
+                             eps.sigma,
+                             n) {
+  
+  eps = eps.mean
+  if (any(eps.sigma != 0)) {
+    eps = extraDistr::rtnorm(n, mean = eps.mean, sd = eps.sigma, a=-Inf, b=(K-theta.true))
+  }
+  X = runif(n, min = theta.true, max = K-eps)
+  
+  W = X + eps
+  return(W)
+}
+
+simulate_datasets = function (config) {
+  df.length = config$n.trials * config$n.error_factors
+  
+  dataset.df = data.frame(
+    error_factor = rep(config$error_factor, length.out = df.length)
+  )
+  
+  W = lapply(dataset.df$error_factor, 
+             function(e) simulate_dataset(theta.true = config$theta.true,
+                                          K = config$K,
+                                          eps.mean = config$dating_error.mean,
+                                          eps.sigma = e * config$fossil.sd,
+                                          n = config$n.samples))
+  
+  dataset.df$W = W
+  return(dataset.df)
+}
+
+estimate_extinction = function (W, sd, method, K, dating_error.mean) {
+  estimate = NA
+  runtime = NA
+  start_time = Sys.time()
+  estimate = switch(
+    method,
+    MLE = mle(W),
+    `BA-MLE` = ba_mle(W, K),
+    Strauss = strauss(W)
+  )
+  runtime = calculate_tdiff(start_time, Sys.time())
+  return(list(point = estimate, point_runtime = runtime))
+}
+
+calculate_tdiff = function(start, end) {
+  as.numeric(difftime(end, start), units = "secs")
+}
+
+estimate_conf_int = function (W,
+                              sd,
+                              method,
+                              alpha,
+                              K,
+                              dating_error.mean) {
+  estimate = switch(
+    method,
+    GRIWM = griwm(
+      alpha = alpha,
+      dates = W,
+      sd = sd,
+      K = K,
+      p_t = alpha,
+      bias_adjusted = F
+    ),
+    `GRIWM-corrected` = griwm(
+      alpha = alpha,
+      dates = W,
+      sd = sd,
+      K = K,
+      p_t = 0.5,
+      bias_adjusted = T
+    ),
+    MINMI = minmi(
+      ages = W,
+      sd = sd,
+      alpha = alpha,
+      K = K
+    )
+  )
+  return(estimate)
+}
+
+mle = function (W) {
+  return(min(W))
+}
+
+ba_mle = function (W, K) {
+  n = length(W)
+  return(min(W) * (n + 1) / n - K / n)
+}
+
+strauss = function (W) {
+  n = length(W)
+  return((n * min(W) - max(W)) / (n - 1))
+}
+
+minmi = function (ages, sd, K, alpha) {
+  start_time = Sys.time()
+  
+  results = tryCatch(
+    {
+      rminmi::minmi(ages, sd, K, alpha)
+    },
+    error = function(cond) {
+      message("Something went wrong with `rminmi`:")
+      message(cond)
+      # Choose a return value in case of error
+      return(list())
+    }
+  )
+  runtime = calculate_tdiff(start_time, Sys.time())
+  return(
+    list(
+      lower = as.numeric(results$lower),
+      point = as.numeric(results$point),
+      upper = as.numeric(results$upper),
+      point_runtime = runtime,
+      conf_int_runtime = runtime,
+      B.lower = results$B$lower,
+      B.point = results$B$point,
+      B.upper = results$B$upper
+    )
+  )
+}
+
+griwm = function(alpha,
+                 dates,
+                 sd,
+                 K,
+                 p_t,
+                 .iter = 10000,
+                 bias_adjusted) {
+  df = data.frame(dates = dates, sd = sd)
+  start_time = Sys.time()
+  results = GRIWM(df, alpha, K, p_t, .iter, bias_adjusted = bias_adjusted)
+  runtime = calculate_tdiff(start_time, Sys.time())
+  return(
+    list(
+      lower = as.numeric(results$lower_ci),
+      point = as.numeric(results$centroid),
+      upper = as.numeric(results$upper_ci),
+      point_runtime = runtime,
+      conf_int_runtime = runtime
+    )
+  )
+}
